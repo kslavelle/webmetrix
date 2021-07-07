f1a2ac5a5641d0a1d2abce5c9b49484447dcdf50
Stefan <stefl141193@gmail.com>
2021-07-07 18:46:15 +0100

    First commit

---
+++ my-ext/src/views/Popup/Auth.js
@@ -0,0 +1,42 @@
+// import React from 'react'
+// import firebase from "firebase/app"
+// import "firebase/auth"
+
+
+// function CreateUser(email, password) {
+//     const auth = firebase.auth().createUserWithEmailAndPassword(email, password)
+//     .then((userCredential) => {
+//     // Signed in 
+//     var user = userCredential.user;
+//     // ...
+//     })
+//     .catch((error) => {
+//     var errorCode = error.code;
+//     var errorMessage = error.message;
+//     // ..
+//     });
+//     return null
+// }
+
+// function SignIn(email, password) {
+//     const auth = firebase.auth().createUserWithEmailAndPassword(email, password)
+//     .then((userCredential) => {
+//     // Signed in 
+//     var user = userCredential.user;
+//     // ...
+//     })
+//     .catch((error) => {
+//     var errorCode = error.code;
+//     var errorMessage = error.message;
+//     // ..
+//     });
+//     return (
+//         <div>
+//             Hello world
+//             <input auth />
+//         </div>
+//     )
+// }
+
+// export default (CreateUser, SignIn)
+
