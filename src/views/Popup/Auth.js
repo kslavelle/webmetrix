5fd3d2fc082434f76ed3d8784c504ade62d8b621
Stefan <stefl141193@gmail.com>
2021-07-11 14:39:41 +0100

    Added baseline functionality to sign in to firebase. All working fine.

--- src/views/Popup/Auth.js
+++ src/views/Popup/Auth.js
@@ -1,38 +1,16 @@
-// function SignIn(email, password) {
-//     const auth = firebase.auth().createUserWithEmailAndPassword(email, password)
-//     .then((userCredential) => {
-//     // Signed in 
-//     var user = userCredential.user;
-//     // ...
-//     })
-//     .catch((error) => {
-//     var errorCode = error.code;
-//     var errorMessage = error.message;
-//     // ..
-//     });
-//     return (
-//         <div>
-//             Hello world
-//             <input auth />
-//         </div>
-//     )
-// }
-
-// export default (CreateUser, SignIn)
-
 import firebase from "firebase";
 import "firebase/auth";
 
 var config = {
-    apiKey: "",
-    authDomain: "",
+    apiKey: "AIzaSyC8kK6cZKx5dCZh2w2-zGV7m-emZGy_2d4",
+    authDomain: "webmetrix-318213.firebaseapp.com",
 };
+
 var app = firebase.initializeApp(config);
 
 function CreateUser(email, password) {
     const auth = firebase.auth().createUserWithEmailAndPassword(email, password)
-    .then((userCredential) => {
-        // Signed in 
+    .then((userCredential) => { 
         var user = userCredential.user;
         console.log(`success: ${user}`);
     })
@@ -44,4 +22,25 @@ function CreateUser(email, password) {
     return null
 }
 
-export default CreateUser;
+function SignInUser(email, password) {
+    // displays 'login' even at sign-up page.
+    const auth = firebase.auth().signInWithEmailAndPassword(email, password)
+    .then((userCredential) => { 
+        var user = userCredential.user;
+        console.log(`success: ${user}`);
+    })
+    .catch(function(error) {
+        // Handle Errors.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // if (errorCode === 'auth/wrong-password') {
+        //   alert('Wrong password.');
+        // } else {
+        //   alert(errorMessage);
+        // }
+        console.log(`error: ${errorMessage}`);
+      });
+}
+
+
+export default (CreateUser, SignInUser);
--- src/views/Popup/login.component.js
+++ src/views/Popup/login.component.js
@@ -1,14 +1,38 @@
-import React, { Component } from "react";
+import { useState } from "react";
+import React from "react";
 
-export default class Login extends Component {
-    render() {
-        return (
-            <form>
-                <h3>Sign In</h3>
-                <input type="email" className="form-control" placeholder="Enter email" />
-                <input type="password" className="form-control" placeholder="Enter password" />
-                <button type="submit" className="btn btn-primary btn-block">Submit</button>
-            </form>
-        );
+import SignInUser from "./Auth";
+
+const LogIn = (props) => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        SignInUser(email, password);
     }
+
+    return (
+        <form>
+            <h3>Sign In</h3>
+            <input
+                type="email"
+                className="form-control"
+                placeholder="Enter email"
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+                type="password"
+                className="form-control"
+                placeholder="Enter password"
+                onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <button onClick={handleSubmit} type="submit" className="btn btn-primary btn-block">Sign In</button>
+        </form>
+    );
 }
+
+export default LogIn
